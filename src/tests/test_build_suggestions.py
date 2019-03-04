56a3ec75def3f7343e1c8446ff8d6b9fbb569896
abdualhag <nestoalhag17fr@yahoo.com>
2019-03-04 14:05:46 -0500

    tests

--- src/tests/test_build_suggestions.py
+++ src/tests/test_build_suggestions.py
@@ -1,2 +1,24 @@
-def test_build_suggestions():
+def test_sum():
     assert 1 + 2 == 3
+    #assert sum([1, 1, 1]) == 6, "Should be 6"
+    
+def _build_suggestions(sentence, results, passages):
+    """Build suggestions based on the a sentence and discovery query."""
+    words = sentence.split(" ")
+    suggestions = []
+    for passage in passages:
+        author = next((result.get("author", None) for result in results if result["id"] == passage["document_id"]), None)
+        suggestion = Suggestion.from_passage(passage, author=author)
+        suggestion.trim_to_contain(words)
+        suggestions.append(suggestion.to_dict())
+    return suggestions
+
+
+def test_build_suggestions():
+    
+    sentence = "Change will not come if we wait for some other person, or if we wait for some other time."
+    passages = []
+    results = []
+    #print (_build_suggestions(sentence, results, passages), "Working")
+    assert len(_build_suggestions(sentence, results, passages)) == 0         
+    
