e63bfed4d35b61d01140499dda500c357014a63b
abdualhag <nestoalhag117fr@yahoo.com>
2019-03-28 10:17:00 -0400

    Keywords

--- src/constants.py
+++ src/constants.py
@@ -3,3 +3,6 @@ DISCOVERY_IAM_URL = "https://gateway.watsonplatform.net/discovery/api"
 
 TONE_ANALYZER_VERSION = "2017-09-21"
 TONE_IAM_URL = "https://gateway-wdc.watsonplatform.net/tone-analyzer/api"
+
+KEYWORDS_ANALYZER_VERSION = "2018-11-16"
+KEYWORDS_IAM_URL = "https://gateway.watsonplatform.net/natural-language-understanding/api"
